921f6ff27ab7393e40c9281b4d03b2b527805cb0
weeellingtonnn <weeellingtonnn@live.com>
2016-04-01 14:20:42 -0300

    Create portalhabbos_config.php

---
+++ portalhabbos_config.php
@@ -0,0 +1,26 @@
+<?php
+
+///////////////////////////////////////////////////////////////
+// # PortalHabbos.biz   API Script -   Release 1.0.3         //
+// # © Copyright PortalHabbos.biz 2016. All rights reserved. //
+///////////////////////////////////////////////////////////////
+
+$_CONFIG = array(
+
+    /* Qual é o "usuario" da sua página na PortalHabbos? */
+    
+    'usuariopagina' => 'Exemplo',
+
+    /* Quantos segundos deve levar para tentar se conectar ao PortalHabbos se offline? */
+
+    'timeout'  => 2,
+
+    /* Você está usando CloudFlare? Se sim, configure para true. */
+
+    'cloudflare' => false,
+
+    /* Se PortalHabbos.biz mudar de dominio, alterar abaixo. */
+
+    'api' => 'http://portalhabbos.biz/'
+
+);
